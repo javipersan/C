1fdffe4fbb333cf84846ca35b5d508614e258d3e
javipersan <92095572+javipersan@users.noreply.github.com>
2026-02-17 12:28:43 +0100

    Add files via upload

---
+++ Comenzamos/mi_hola_mundo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ slñkkslñkdfsñlksdflñksdf
+sdñlfksdñlkfsdlñksdf
+sñlkfñlsklñksdf
+sñlfksñlkfsd
+
+*/
+int main() 
+{
+	
+	printf ("hola mundo");
+	
+	system ("pause");
+	
+	system("cls");
+	
+	return 0;
+}
+
---
+++ Comenzamos/tamanio_tipos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	
+	printf ("nº de byte variable tipo char %d\n",sizeof(char));	
+	printf ("nº de byte variable tipo int %d\n",sizeof(int));	
+	printf ("nº de byte variable tipo short %d\n",sizeof(short));
+	printf ("nº de byte variable tipo long %d\n",sizeof(long));
+	return 0;
+}
+
